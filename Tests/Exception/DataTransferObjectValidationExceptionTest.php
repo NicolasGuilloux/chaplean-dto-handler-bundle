173fc514b7296fce577a9458a579130be9b7c90c
Nicolas Guilloux <nguilloux@richcongress.com>
2019-09-13 16:25:25 +0200

    Prepare for the 2.0.0

---
+++ Tests/Exception/DataTransferObjectValidationExceptionTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Chaplean\Bundle\DtoHandlerBundle\Tests\Exception;
+
+use Chaplean\Bundle\DtoHandlerBundle\Exception\DataTransferObjectValidationException;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+/**
+ * Class DataTransferObjectValidationExceptionTest
+ *
+ * @package   Chaplean\Bundle\DtoHandlerBundle\Tests\Exception
+ * @author    Nicolas Guilloux <nguilloux@richcongress.com>
+ * @copyright 2014 - 2019 RichCongress (https://www.richcongress.com)
+ */
+class DataTransferObjectValidationExceptionTest extends TestCase
+{
+    /**
+     * @covers \Chaplean\Bundle\DtoHandlerBundle\Exception\DataTransferObjectValidationException::__construct()
+     * @covers \Chaplean\Bundle\DtoHandlerBundle\Exception\DataTransferObjectValidationException::getViolations()
+     * @covers \Chaplean\Bundle\DtoHandlerBundle\Exception\DataTransferObjectValidationException::getViolationsArray()
+     *
+     * @return void
+     */
+    public function testConstructorAndGetters(): void
+    {
+        $violation1 = new ConstraintViolation('Bad Value 1', null, [], 'badValue1', 'violation1', 'badValue1');
+        $violation2 = new ConstraintViolation('Bad Value 2', null, [], 'badValue2', 'violation2', 'badValue2');
+
+        $violations = new ConstraintViolationList(
+            [
+                $violation1,
+                $violation2
+            ]
+        );
+
+        $exception = new DataTransferObjectValidationException($violations);
+        $expectedArray = [
+            'violation1' => 'Bad Value 1',
+            'violation2' => 'Bad Value 2',
+        ];
+
+        self::assertSame($violations, $exception->getViolations());
+        self:self::assertEquals($expectedArray, $exception->getViolationsArray());
+    }
+}
---
+++ Tests/Resources/DTO/DummyDataTransferObject.php
@@ -0,0 +1,107 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the DtoHandlerBundle package.
+ *
+ * (c) Chaplean.coop <contact@chaplean.coop>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\DTO;
+
+use Chaplean\Bundle\DtoHandlerBundle\Annotation\DTO;
+use Chaplean\Bundle\DtoHandlerBundle\Annotation\MapTo;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Validator\Constraints as Assert;
+use Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\Entity\DummyEntity;
+use Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\DTO\SubDataTransferObject;
+
+/**
+ * Class DummyDataTransferObject
+ *
+ * @package   Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\DTO
+ * @author    Nicolas - Chaplean <nicolas@chaplean.coop>
+ * @copyright 2014 - 2019 Chaplean (https://www.chaplean.coop)
+ *
+ * @DTO
+ */
+final class DummyDataTransferObject
+{
+    /**
+     * @var DummyEntity
+     */
+    public $targetEntity;
+
+    /**
+     * @var string
+     */
+    public $property1;
+
+    /**
+     * @var integer
+     *
+     * @Assert\NotNull
+     * @Assert\Type("integer")
+     */
+    public $property2;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\Type("Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\Entity\DummyEntity")
+     * @MapTo("keyname")
+     */
+    public $property3;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\Type("Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\Entity\DummyEntity")
+     * @ParamConverter(name="property4", class="\Chaplean\Bundle\DtoHandlerBundle\Tests\Resources\Entity\DummyEntity")
+     */
+    public $property4;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\All(@Assert\Type("Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\Entity\DummyEntity"))
+     * @MapTo("keyname")
+     */
+    public $property5;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\All(
+     *     @Assert\NotNull
+     * )
+     */
+    public $property6;
+
+    /**
+     * @var SubDataTransferObject
+     *
+     * @Assert\All(
+     *     @Assert\Type("Tests\Chaplean\Bundle\DtoHandlerBundle\Resources\DTO\SubDataTransferObject")
+     * )
+     * @Assert\Valid
+     */
+    public $property7;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\NotNull
+     * @Assert\DateTime
+     */
+    public $property8;
+
+    /**
+     * @var DummyEntity
+     *
+     * @Assert\Date
+     */
+    public $property9;
+}
